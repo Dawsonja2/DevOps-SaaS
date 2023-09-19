6c6bf97cd9c13e0d06f1cd6f182e20df7952ace8
Dawsonja2 <59925930+Dawsonja2@users.noreply.github.com>
2023-09-18 21:47:53 -0600

    Add files via upload

---
+++ SoftwareAsAService-DevOps/http.sh
@@ -0,0 +1,2 @@
+http -A digest -a vcu:rams GET http://127.0.0.1:5000/pong
+http -A digest -a vcu:rams GET http://127.0.0.1:5001/ping
---
+++ SoftwareAsAService-DevOps/pingservice/ping.py
@@ -0,0 +1,35 @@
+import http
+import time
+from flask import Flask, jsonify, request
+from flask_httpauth import HTTPDigestAuth
+import requests
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret key here'
+auth = HTTPDigestAuth()
+
+users = {
+    'vcu': 'rams'
+}
+
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+
+@app.route('/ping', methods=['GET'])
+@auth.login_required
+def ping():
+    start = time.perf_counter()
+    response = requests.get(
+        "https://ping-service-app-jared-dawson.herokuapp.com/ping")
+    request_time = time.perf_counter() - start
+    return "Request completed in {0:.0f}ms".format(request_time)
+
+
+if __name__ == '__main__':
+    app.run()  # host='0.0.0.0', port=5000
